b5aa26bea4de5c41fafd674aa7528ddda541899c
Nathan Ross <NathanAnthonyRoss19@gmail.com>
2019-02-12 17:49:30 -0800

    I included Binaryen's wasm-opt utillity into the build chain

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
-RUSTFLAGS="-C link-arg=--strip-debug -C link-arg=-zstack-size=32768" cargo +nightly build --target wasm32-unknown-unknown --release \
-&& ./small-wasm-trimmer --remove-sections 0 4 --remove-exports "__heap_base" "__data_end" < target/wasm32-unknown-unknown/release/rust_wasm_test.wasm > out.wasm 2>/dev/null \
-&& wc -c out.wasm
+RUSTFLAGS="-C link-arg=--strip-debug -C link-arg=-zstack-size=32768" cargo +nightly build --target wasm32-unknown-unknown --release
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+#I do not intend to load more than one module at a time, so these two exports are not necessary
+#this command also removes all custom sections by default, namely the producer's section
+#Binaryen has no easy way to specify which exports are unwanted, so I use this small utility instead.
+./small-wasm-trimmer --remove-exports "__heap_base" "__data_end" < target/wasm32-unknown-unknown/release/rust_wasm_test.wasm > out.wasm 2>/dev/null
+
+if [[ $1 != "--skip-wasm-opt" ]]; then
+    if [[ -x wasm-opt ]]; then
+        #use Binaryen's wasm-opt utility to strip out unused globals, functions, etc
+        ./wasm-opt out.wasm -Oz -o out.wasm
+    else
+        echo "Please compile the binaryen utility wasm-opt https://github.com/WebAssembly/binaryen and place the binary in this directory."
+        echo "Use the argument \"--skip-wasm-opt\" if you want a less optimized wasm module."
+    fi
+fi
--- setup-toolchain.sh
+++ setup-toolchain.sh
@@ -8,3 +8,7 @@ if command -v rustup > /dev/null; then
 else
     echo "The rust toolchain is missing.  Please install it from https://sh.rustup.rs"
 fi
+
+if [ ! -x wasm-opt ]; then
+    echo "Please compile the binaryen utility wasm-opt https://github.com/WebAssembly/binaryen and place the binary in this directory."
+fi
